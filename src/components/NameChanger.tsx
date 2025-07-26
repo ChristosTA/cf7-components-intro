0ba2c4e0a48573e75c724d5bcc851541075a7fbe
Christos Tartaris <xristosbnd@gmail.com>
2025-07-26 21:34:05 +0300

    feat: new components created

---
+++ src/components/NameChanger.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const NameChanger =() => {
+    const [name, setName] = React.useState("");
+
+    const handleChange= (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
+    }
+
+
+
+    return (
+        <>
+            <h1 className="text-center text-xl pt-4">Hello, {name || "Stranger"}</h1>
+            <div className="text-center mt-4">
+                <input type="text"
+                       value={name}
+                       className="border px-4 py-2"
+                       onChange={handleChange}
+                />
+            </div>
+        </>
+    )
+}
+export default NameChanger;
